f576b57ae52484d8557843eacaa1fbe37b87eaf6
Javier <javierjpagan@gmail.com>
2020-01-11 22:44:56 -0500

    Testing

--- src/components/pages/landing/index.js
+++ src/components/pages/landing/index.js
@@ -58,9 +58,6 @@ export default class Landing extends Component {
 
       </Container>
 
-      // <div>
-      //   <h5 className="text-center mt-2 mb-2">Learn how your daily actions impact your carbon footprint</h5>
-      // </div>
     );
   }
 }
